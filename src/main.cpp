4d063b1d305698509f55c94cdf834c28abfd78a4
Simon Busch <morphis@gravedo.de>
2013-07-29 21:00:12 +0000

    Implement static handling of the XDG_RUNTIME_DIR directory
    In normal desktop situations XDG_RUNTIME_DIR would be provided when launching a user
    session by the environment but as we're not in such a case (yet?) we have to take care
    about this on our own. Sadly the site effect is that we have to tell every single launched
    application about the correct path.
    
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/main.cpp
+++ src/main.cpp
@@ -17,79 +17,19 @@
 
 
 #include <QGuiApplication>
-#include <QFileInfo>
 #include <QRect>
 #include <QScreen>
+#include <QDir>
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 #include <systemd/sd-daemon.h>
 
 #include "compositor.h"
 
-static int convertPermission(const QFileInfo &fileInfo)
-{
-    int p = 0;
-
-    if (fileInfo.permission(QFile::ReadUser))
-        p += 400;
-    if (fileInfo.permission(QFile::WriteUser))
-        p += 200;
-    if (fileInfo.permission(QFile::ExeUser))
-        p += 100;
-    if (fileInfo.permission(QFile::ReadGroup))
-        p += 40;
-    if (fileInfo.permission(QFile::WriteGroup))
-        p += 20;
-    if (fileInfo.permission(QFile::ExeGroup))
-        p += 10;
-    if (fileInfo.permission(QFile::ReadOther))
-        p += 4;
-    if (fileInfo.permission(QFile::WriteOther))
-        p += 2;
-    if (fileInfo.permission(QFile::ExeOther))
-        p += 1;
-
-    return p;
-}
-
-static void verifyXdgRuntimeDir()
-{
-    QByteArray dirName = qgetenv("XDG_RUNTIME_DIR");
-
-    if (dirName.isEmpty()) {
-        QString msg = QObject::tr(
-                    "The XDG_RUNTIME_DIR environment variable is not set.\n"
-                    "Refer to your distribution on how to get it, or read\n"
-                    "http://www.freedesktop.org/wiki/Specifications/basedir-spec\n"
-                    "on how to implement it.\n");
-        qFatal(msg.toUtf8());
-    }
-
-    QFileInfo fileInfo(dirName);
-
-    if (!fileInfo.exists()) {
-        QString msg = QObject::tr(
-                    "The XDG_RUNTIME_DIR environment variable is set to "
-                    "\"%1\", which doesn't exist.\n").arg(dirName.constData());
-        qFatal(msg.toUtf8());
-    }
-
-    if (convertPermission(fileInfo) != 700 || fileInfo.ownerId() != getuid()) {
-        QString msg = QObject::tr(
-                    "XDG_RUNTIME_DIR is set to \"%1\" and is not configured correctly.\n"
-                    "Unix access mode must be 0700, but is 0%2.\n"
-                    "It must also be owned by the current user (UID %3), "
-                    "but is owned by UID %4 (\"%5\").\n")
-                .arg(dirName.constData())
-                .arg(convertPermission(fileInfo))
-                .arg(getuid())
-                .arg(fileInfo.ownerId())
-                .arg(fileInfo.owner());
-        qFatal(msg.toUtf8());
-    }
-}
+#define XDG_RUNTIME_DIR_DEFAULT "/tmp/luna-session"
 
 int main(int argc, char *argv[])
 {
@@ -102,7 +42,14 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    verifyXdgRuntimeDir();
+    QDir xdgRuntimeDir(XDG_RUNTIME_DIR_DEFAULT);
+
+    // cleanup old sessions and recreate
+    if (xdgRuntimeDir.exists())
+        xdgRuntimeDir.removeRecursively();
+
+    mkdir(XDG_RUNTIME_DIR_DEFAULT, 0700);
+    setenv("XDG_RUNTIME_DIR", XDG_RUNTIME_DIR_DEFAULT, 0);
 
     luna::Compositor compositor;
     compositor.setTitle(QLatin1String("LunaNext"));
