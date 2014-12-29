cb13d6081cdf93d00fab9a754ae98304346be8ed
Simon Busch <morphis@gravedo.de>
2014-12-29 12:53:56 +0100

    tools: recorder: add simple screencast recorder utility
    Taken from https://github.com/nemomobile/lipstick-recorder
    
    Signed-off-by: Simon Busch <morphis@gravedo.de>

---
+++ tools/recorder/main.cpp
@@ -0,0 +1,26 @@
+/***************************************************************************
+**
+** Copyright (C) 2014 Jolla Ltd.
+** Contact: Giulio Camuffo <giulio.camuffo@jollamobile.com>
+**
+** This file is part of lipstick-recorder.
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
+#include <QGuiApplication>
+
+#include "recorder.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    Recorder recorder;
+    return app.exec();
+}
