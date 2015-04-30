c5cddec0ce94ac8735b29287f89392e1f252cfe1
Herrie <Github.com@herrie.org>
2015-04-30 16:42:53 +0200

    Proper bracket usage

--- plugins/common/fontutils.cpp
+++ plugins/common/fontutils.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright 2013 Canonical Ltd.
- * Copyright 2015 Herman van Hazendonk (github.com@herrie.org)
+ * Copyright 2015 Herman van Hazendonk <github.com@herrie.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
